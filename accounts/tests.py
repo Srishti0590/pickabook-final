a6aeda654d9e1a9c2e06313a696dcbe7a25142f3
RitikaDongol <reetica123@gmail.com>
2022-02-27 03:12:04 +0545

    unit test

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+
+from django.urls import reverse, resolve
+from accounts.views import login, profile, password_change_user
 
 # Create your tests here.
+class URLTests(SimpleTestCase):
+    def test_login(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func, login)
+
+    def test_profile(self):
+        url = reverse('profile')
+        self.assertEquals(resolve(url).func, profile)
+
+    def test_change_password(self):
+        url = reverse('change')
+        self.assertEquals(resolve(url).func, password_change_user)
+
+
