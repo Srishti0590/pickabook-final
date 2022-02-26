865abc81ce812c6bab62f6e82a7223da9cfcfad1
RitikaDongol <reetica123@gmail.com>
2022-02-27 03:09:02 +0545

    model edit

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,12 @@ class Profile(models.Model):
     # def __str__(self):
     #     return self.username
 
+class Book(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    profile_pic = models.FileField(upload_to='static/profile', default='static/default_user.jpg')
+    created_date = models.DateTimeField(auto_now_add=True)
